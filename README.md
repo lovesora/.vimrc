f06aa8acd4ab9a5ac49f8a5ebbd6fd0423bfe1e0
liuxin <475212506@qq.com>
2017-12-18 19:15:36 +0800

    add update-to-vim8.sh

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # .vimrc
 my vim configuration
+
+# update to vim8 in centos7
+```bash
+$ ./update-to-vim8.sh
+```
+
---
+++ update-to-vim8.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+yum remove vim -y
+
+# 如果失败，提示unknown host apt.sw.be，手工安装：
+# wget http://mirror.centos.org/centos/7/os/x86_64/Packages/ncurses-devel-5.9-13.20130511.el7.x86_64.rpm
+# yum install ncurses-devel-5.9-13.20130511.el7.x86_64.rpm
+yum install ncurses-devel -y
+
+git clone https://github.com/vim/vim.git
+cd vim/src
+
+make
+make install
+
+cd ../..
+rm -rf vim
+
+echo '#!/bin/bash' > /usr/local/bin/vim /etc/profile.d/path.sh && echo 'export PATH=$PATH:/usr/local/bin/vim' >> /usr/local/bin/vim /etc/profile.d/path.sh
+
+source /etc/profile.d/path.sh
+
